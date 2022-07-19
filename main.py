0c3867825b62537671b1c65275ced705da15bfa4
Mohamed Shalaby <mhdshalabystar@gmail.com>
2022-07-20 02:39:51 +0300

    starter code

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 import typer
-
+import json
+from pathlib import Path
 app = typer.Typer()
 
 @app.command()
-def hi():
+def hi()->str:
+    names = Path("names.json")
     name = typer.prompt("What is your name?")
     typer.secho(f"Hi, {name}!",
             fg=typer.colors.GREEN)
+    if names.is_file():
+        with open(names,"r") as names_f:
+            data = json.load(names_f)
+        data["names"].append(name)
+        with open(names,"w") as names_f:
+            json.dump(data,names_f, indent=4)
+    else:
+        data = {
+                "names":[name]
+                }
+        with open(names,"w") as names_f:
+            json.dump(data,names_f,indent=4)
+
+@app.command()
+def bye():
+    check_name = Path("names.json")
+    if check_name.is_file():
+        with open(check_name, "r") as names_f:
+            data = json.load(names_f)
+        for name in data["names"]:
+            typer.secho(f"Goodbye {name} see you later.",
+                    fg=typer.colors.YELLOW)
+        check_name.unlink()
+    else:
+        typer.secho("Goodbye buddy see you later.",
+                fg=typer.colors.YELLOW)
+
 
 if __name__ == "__main__":
     app()
